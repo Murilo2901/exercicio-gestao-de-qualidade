e5ae049ff33438fa193d61a1ff69141158dd6dd2
MURILO KERSCHBAUM <murilo_kerschbaum@estudante.sesisenai.org.br>
2025-11-25 19:40:22 -0300

    Terminado

---
+++ exercicio-gestao-de-qualidade-main/src/main/java/org/example/service/equipamento/EquipamentoServiceImpl.java
@@ -0,0 +1,34 @@
+package org.example.service.equipamento;
+
+import org.example.model.Equipamento;
+import org.example.respositorio.EquipamentoRepositorio;
+
+import java.sql.SQLException;
+
+public class EquipamentoServiceImpl implements EquipamentoService {
+
+    EquipamentoRepositorio repositorio = new EquipamentoRepositorio();
+
+    @Override
+    public Equipamento criarEquipamento(Equipamento equipamento) throws SQLException {
+        equipamento.setStatusOperacional("OPERACIONAL");
+        equipamento = repositorio.salvarEquipamento(equipamento);
+
+        if (equipamento.getId() == null){
+            throw new RuntimeException("Ocorreu um erro!");
+        }
+
+        return equipamento;
+    }
+
+    @Override
+    public Equipamento buscarEquipamentoPorId(Long id) throws SQLException {
+        Equipamento equipamento = repositorio.buscarEquipamentoPorId(id);
+
+        if(equipamento == null){
+            throw  new RuntimeException("Equipamento não encontrado!");
+        }
+
+        return equipamento;
+    }
+}
---
+++ exercicio-gestao-de-qualidade-main/src/main/java/org/example/service/falha/FalhaServiceImpl.java
@@ -0,0 +1,42 @@
+package org.example.service.falha;
+
+import org.example.model.Equipamento;
+import org.example.model.Falha;
+import org.example.respositorio.EquipamentoRepositorio;
+import org.example.respositorio.FalhaRepositorio;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class FalhaServiceImpl implements FalhaService{
+
+    EquipamentoRepositorio equipamentoRepositorio = new EquipamentoRepositorio();
+
+    FalhaRepositorio falhaRepositorio = new FalhaRepositorio();
+
+    @Override
+    public Falha registrarNovaFalha(Falha falha) throws SQLException {
+        if(!equipamentoRepositorio.equipamentoExiste(falha.getEquipamentoId())){
+            throw new IllegalArgumentException("Equipamento não encontrado!");
+        }
+
+        falha.setStatus("ABERTA");
+
+        falha = falhaRepositorio.criarFalha(falha);
+
+        if(falha.getCriticidade() == "CRITICA"){
+            equipamentoRepositorio.atualizarStatus(falha.getEquipamentoId(),"EM_MANUTENCAO");
+        }
+
+        if(falha.getId() == null){
+            throw new RuntimeException("Erro ao salvar falha!");
+        }
+
+        return falha;
+    }
+
+    @Override
+    public List<Falha> buscarFalhasCriticasAbertas() throws SQLException {
+        return falhaRepositorio.buscarFalhasCriticasAbertas();
+    }
+}
